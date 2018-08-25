0702e6ba421761c4fbce9c95c8d700db0d3dd58b
convexbrain <convexbrain@gmail.com>
2018-08-25 19:31:23 +0900

    updated: a way of indexing.

--- sandbox/TopolOpt/3dMaxStiff/main.py
+++ sandbox/TopolOpt/3dMaxStiff/main.py
@@ -10,60 +10,54 @@ import itertools as itt
 # y +:back, -:front
 # z +:top, -:bottom
 
-class DofMat:
-    NDIM = 3
+class FeMat:
     #
-    def __init__(self, nx, ny, nz):
-        self.nx = nx
-        self.ny = ny
+    def __init__(self, nz, ny, nx):
         self.nz = nz
-        nvars = DofMat.NDIM * nx * nx * nz
-        self.mat = lil_matrix((nvars, nvars))
-    def neibors(self, x, y, z):
-        nx = self.nx
-        ny = self.ny
-        nodes = np.empty(0)
-        for oz in range(2):
-            for oy in range(2):
-                for ox in range(2):
-                    for dim in range(DofMat.NDIM):
-                        nodes = np.append(nodes, dim + DofMat.NDIM * ((x + ox) + nx * ((y + oy) + ny * (z + oz))))
-        return self.mat[np.ix_(nodes, nodes)]
+        self.ny = ny
+        self.nx = nx
+        nq = nz * ny * nx * 3
+        self.m = lil_matrix((nq, nq))
+    def submat(self, nnodes):
+        q_n = np.empty(0)
+        for node in nnodes:
+            serpos = (node[0] * self.ny + node[1]) * self.nx + node[2]
+            for dim in range(3):
+                q_n = np.append(q_n,  serpos * 3 + dim)
+        return self.m[np.ix_(q_n, q_n)]
 #
-class DofVec:
+class FeVec:
     #
-    def __init__(self, nx, ny, nz):
-        self.nx = nx
-        self.ny = ny
+    def __init__(self, nz, ny, nx):
         self.nz = nz
-        nvars = 3 * nx * nx * nz
-        self.mat = lil_matrix((nvars, 1))
+        self.ny = ny
+        self.nx = nx
+        nq = nz * ny * nx * 3
+        self.m = lil_matrix((nq, 1))
 #
 class TopolOpt3D:
     #
     @classmethod
-    def idx_neibors(cls, x, y, z):
-        indices = np.empty(0)
-        for oz in range(2):
-            for oy in range(2):
-                for ox in range(2):
-                    indices = np.append(indices, np.array([x + ox, y + oy, z + oz]))
-        return indices.reshape((2 ** 3, 3))
+    def neibor_nodes(cls, elem):
+        nodes = np.empty(0)
+        for ofst in np.ndindex(2, 2, 2):
+            nodes = np.append(nodes, np.array(elem) + np.array(ofst))
+        return nodes.reshape((2 ** 3, 3))
     #
-    def solve(self, (nx, ny, nz), slen, vratio):
+    def solve(self, (nz, ny, nx), slen, vratio):
         # Configuration
+        self.nz = nz
         self.nx = nx
         self.ny = ny
-        self.nz = nz
         self.slen = slen
         max_i = 10
         # Initialization
-        rho = vratio * np.ones((self.nx, self.ny, self.nz))
+        rho = vratio * np.ones((self.nz, self.ny, self.nx))
         # Iteration
         i = 0
         while i < max_i: # TODO
-            u, l = self.fe_ana(rho)
-            dldr = self.fe_sens(u, rho)
+            u, l = self.ana_fe(rho)
+            dldr = self.calc_sens(u, rho)
             rho_new = self.update_oc(rho, dldr)
             #
             print('{0:5}: l:{1}'.format(i, l))
@@ -73,32 +67,37 @@ class TopolOpt3D:
         #
         return rho
     #
-    def fe_ana(self, rho):
-        K = DofMat(self.nx + 1, self.ny + 1, self.nz + 1)
+    def ana_fe(self, rho):
+        K = FeMat(self.nz + 1, self.ny + 1, self.nx + 1)
         #
-        for x, y, z in itt.product(range(self.nx), range(self.ny), range(self.nz)):
-            r_n = TopolOpt3D.idx_neibors(x, y, z) * self.slen
-            K_n = K.neibors(x, y, z)
-            K_n += el_stiff(rho[x, y, z], r_n)
+        for elem in np.ndindex(self.nz, self.ny, self.nx):
+            nnodes = TopolOpt3D.neibor_nodes(elem)
+            r_n = nnodes * self.slen
+            K_n = K.submat(nnodes)
+            K_n += self.stiff(rho[elem], r_n)
             # TODO
         #
-        f = DofVec(self.nx + 1, self.ny + 1, self.nz + 1)
+        f = FeVec(self.nx + 1, self.ny + 1, self.nz + 1)
         #
-        u = DofVec(self.nx + 1, self.ny + 1, self.nz + 1)
+        u = FeVec(self.nx + 1, self.ny + 1, self.nz + 1)
         #
         l = 0.0
         #
         return u, l
     #
-    def fe_sens(self, u, rho):
+    def calc_sens(self, u, rho):
         dldr = np.zeros((self.nx + 1, self.ny + 1, self.nz + 1))
         # TODO
         return dldr
     #
     def update_oc(self, rho, dldr):
-        rho_new = np.zeros((self.nx, self.ny, self.nz))
+        rho_new = np.zeros_like(rho)
         # TODO
         return rho_new
+    #
+    def stiff(self, rho_e, r_n):
+        # TODO
+        return np.zeros((r_n.size, r_n.size))
 #
 #####
 #
