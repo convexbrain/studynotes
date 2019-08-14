2bdf3af626454be966a08396f0c140a54ee2fb1e
convexbrain <convexbrain@gmail.com>
2019-08-14 20:53:37 +0900

    updated: armtest

--- sandbox/nRF52840/rust/armtest/src/main.rs
+++ sandbox/nRF52840/rust/armtest/src/main.rs
@@ -9,6 +9,7 @@ use nrf52840_hal::nrf52840_pac::Peripherals;
 use nrf52840_hal::gpio::*;
 //use nrf52840_hal::prelude::*;
 use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::InputPin;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -22,10 +23,15 @@ fn main() -> ! {
     let p = Peripherals::take().unwrap();
     let port0 = p.P0.split();
 
-    let mut led1 = port0.p0_07.into_push_pull_output(Level::High);
-    led1.set_high().unwrap();
+    let mut led = port0.p0_07.into_push_pull_output(Level::Low);
+    let sw = port0.p0_13.into_floating_input();
 
     loop {
-        asm::bkpt();
+        if sw.is_high().unwrap() {
+            led.set_low().unwrap();
+        }
+        else {
+            led.set_high().unwrap();
+        }
     }
 }
