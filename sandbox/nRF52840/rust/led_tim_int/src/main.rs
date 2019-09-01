0e2bbb637197463a1fff33c152efddb15643ca18
convexbrain <convexbrain@gmail.com>
2019-09-01 16:35:51 +0900

    created: led_tim_int

---
+++ sandbox/nRF52840/rust/led_tim_int/src/main.rs
@@ -0,0 +1,61 @@
+#![no_main]
+#![no_std]
+
+#![allow(deprecated)] // to suppress "warning: use of deprecated item ... Users should use the traits in digital::v2.""
+
+use core::panic::PanicInfo;
+use cortex_m::asm;
+use cortex_m_rt::entry;
+
+use nrf52840_hal::target::Peripherals;
+use nrf52840_hal::target::CorePeripherals;
+use nrf52840_hal::gpio::*;
+use nrf52840_hal::gpio::p0::*;
+use nrf52840_hal::prelude::*;
+use nrf52840_hal::target::interrupt;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        asm::bkpt();
+    }
+}
+
+static mut O_LED: Option<P0_07<Output<PushPull>>> = None;
+static mut O_SW: Option<P0_13<Input<Floating>>> = None;
+
+#[entry]
+fn main() -> ! {
+    let p = Peripherals::take().unwrap();
+    let port0 = p.P0.split();
+
+    unsafe {
+        O_LED = Some(port0.p0_07.into_push_pull_output(Level::Low));
+        O_SW = Some(port0.p0_13.into_floating_input());
+    }
+
+    let mut cp = CorePeripherals::take().unwrap();
+
+    let mut t = p.TIMER0.constrain();
+    t.enable_interrupt(&mut cp.NVIC);
+    t.start(1000000u32);
+
+    loop {}
+}
+
+#[interrupt]
+fn TIMER0() -> ! {
+    unsafe {
+        let led = O_LED.as_mut().unwrap();
+        let sw = O_SW.as_mut().unwrap();
+
+        loop {
+            if sw.is_high() {
+                led.set_low();
+            }
+            else {
+                led.set_high();
+            }
+        }
+    }
+}
