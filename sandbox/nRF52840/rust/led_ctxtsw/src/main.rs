9bf75c2fd8cce273b26deb9414606c80922de4d5
convexbrain <convexbrain@gmail.com>
2019-09-15 16:45:55 +0900

    updated: led_ctxtsw

--- sandbox/nRF52840/rust/led_ctxtsw/src/main.rs
+++ sandbox/nRF52840/rust/led_ctxtsw/src/main.rs
@@ -1,3 +1,5 @@
+// UNDER DEVELOPMENT AND EXPERIMENT
+
 #![no_main]
 #![no_std]
 #![feature(asm)]
@@ -17,12 +19,43 @@ static mut O_TIMER0: Option<TIMER0> = None;
 
 static mut LED_CNT: u32 = 64_000_000 / 2;
 
-struct Task
+static mut STACK0: [usize; 1024] = [0; 1024];
+static mut STACK1: [usize; 1024] = [0; 1024];
+static mut STACK2: [usize; 1024] = [0; 1024];
+
+struct TaskMgr
 {
     f0: *const fn() -> !,
     f1: *const fn() -> !,
     f2: *const fn() -> !,
+    sp0: *mut usize,
+    sp1: *mut usize,
+    sp2: *mut usize,
+    tid: Option<usize>,
+    num_tasks: usize,
 }
+
+impl TaskMgr
+{
+    fn setup(self) -> Self
+    {
+        let ret_addr = self.sp0 as usize + (8 + 7) * 4;
+        let ret_addr = ret_addr as *mut usize;
+        unsafe { *ret_addr = self.f0 as usize }
+
+        let ret_addr = self.sp1 as usize + (8 + 7) * 4;
+        let ret_addr = ret_addr as *mut usize;
+        unsafe { *ret_addr = self.f1 as usize }
+
+        let ret_addr = self.sp2 as usize + (8 + 7) * 4;
+        let ret_addr = ret_addr as *mut usize;
+        unsafe { *ret_addr = self.f2 as usize }
+
+        self
+    }
+}
+
+static mut O_TASKMGR: Option<TaskMgr> = None;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -33,6 +66,23 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[entry]
 fn main() -> ! {
+    {
+        unsafe {
+            O_TASKMGR = Some(
+                TaskMgr {
+                    f0: led_tgl as *const fn() -> !,
+                    f1: led_slow as *const fn() -> !,
+                    f2: led_fast as *const fn() -> !,
+                    sp0: &mut STACK0[1024 - 128],
+                    sp1: &mut STACK1[1024 - 128],
+                    sp2: &mut STACK2[1024 - 128],
+                    tid: None,
+                    num_tasks: 3,
+                }.setup()
+            );
+        }
+    }
+
     let peri = Peripherals::take().unwrap();
     let mut cperi = CorePeripherals::take().unwrap();
 
@@ -68,18 +118,9 @@ fn main() -> ! {
         let cycles = 1_000_000;
         timer0.cc[0].write(|w| unsafe { w.cc().bits(cycles) }); // 1 sec
         timer0.tasks_clear.write(|w| w.tasks_clear().set_bit());
-        timer0.tasks_start.write(|w| w.tasks_start().set_bit());
+        //timer0.tasks_start.write(|w| w.tasks_start().set_bit()); // TODO
         //
         unsafe { O_TIMER0 = Some(timer0) }
-    }
-
-    {
-        // TODO
-        let t = Task {
-            f0: led_tgl as *const fn() -> !,
-            f1: led_slow as *const fn() -> !,
-            f2: led_fast as *const fn() -> !,
-        };
     }
 
     loop {}
@@ -97,6 +138,8 @@ fn led_tgl() -> !
             p0.outset.write(|w| w.pin7().set_bit());
             asm::delay(LED_CNT);
         }
+        
+        req_task_switch();
     }
 }
 
@@ -140,8 +183,49 @@ fn TIMER0()
 }
 
 #[no_mangle]
-pub extern fn task_switch(curr_sp: u32) -> u32
+pub extern fn task_switch(curr_sp: *mut usize) -> *mut usize
 {
-    // TODO
-    curr_sp
+    let next_sp;
+
+    unsafe {
+        let mut t = O_TASKMGR.as_mut().unwrap();
+
+        let next_tid;
+
+        if let Some(curr_tid) = t.tid {
+            if curr_tid == 0 {
+                t.sp0 = curr_sp;
+            }
+            else if curr_tid == 1 {
+                t.sp1 = curr_sp;
+            }
+            else if curr_tid == 2 {
+                t.sp2 = curr_sp;
+            }
+            else {
+                panic!();
+            }
+
+            next_tid = if curr_tid + 1 < t.num_tasks {curr_tid + 1} else {0};
+        }
+        else {
+            next_tid = 0;
+        };
+
+        t.tid = Some(next_tid);
+        if next_tid == 0 {
+            next_sp = t.sp0;
+        }
+        else if next_tid == 1 {
+            next_sp = t.sp1;
+        }
+        else if next_tid == 2 {
+            next_sp = t.sp2;
+        }
+        else {
+            panic!();
+        }
+    }
+
+    next_sp
 }
