597c40cff4b2992ef3280f117929e0c13b2dc7af
convexbrain <convexbrain@gmail.com>
2023-11-04 20:43:21 +0900

    updated 063bk

---
+++ atcoder/typical90/typical90_063bk/src/main.rs
@@ -0,0 +1,147 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::*,
+    rc::*, cell::*, ops::Bound::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+#[derive(Debug, Clone)]
+struct Vec2D<T> {
+    vec_row_major: Vec<T>,
+    nrows: usize,
+    ncols: usize,
+}
+
+impl<T> Vec2D<T> {
+    fn from(vec_row_major: Vec<T>, (nrows, ncols): (usize, usize)) -> Self {
+        assert!(vec_row_major.len() >= nrows * ncols);
+        Self {vec_row_major, nrows, ncols}
+    }
+
+    fn release(self) -> Vec<T> {
+        self.vec_row_major
+    }
+}
+
+impl<T> Vec2D<T> {
+    fn get(&self, row: isize, col: isize) -> Option<&T> {
+        if row < 0 || col < 0 || !(row < self.nrows as isize) || !(col < self.ncols as isize) {
+            None
+        }
+        else {
+            Some(&self.vec_row_major[row as usize * self.ncols + col as usize])
+        }
+    }
+
+    fn get_mut(&mut self, row: isize, col: isize) -> Option<&mut T> {
+        if row < 0 || col < 0 || !(row < self.nrows as isize) || !(col < self.ncols as isize) {
+            None
+        }
+        else {
+            Some(&mut self.vec_row_major[row as usize * self.ncols + col as usize])
+        }
+    }
+}
+
+impl<T> Index<(usize, usize)> for Vec2D<T> {
+    type Output = T;
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        assert!(index.0 < self.nrows);
+        assert!(index.1 < self.ncols);
+        &self.vec_row_major[index.0 * self.ncols + index.1]
+    }
+}
+
+impl<T> IndexMut<(usize, usize)> for Vec2D<T> {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
+        assert!(index.0 < self.nrows);
+        assert!(index.1 < self.ncols);
+        &mut self.vec_row_major[index.0 * self.ncols + index.1]
+    }
+}
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+    let mut token = buf.split_whitespace();
+
+    let h: usize = token.next().unwrap().parse().unwrap();
+    let w: usize = token.next().unwrap().parse().unwrap();
+
+    debug!(h, w);
+
+    let p: Vec<u32> = (0..(h * w)).map(|_| {
+        token.next().unwrap().parse().unwrap()
+    }).collect();
+
+    debug!(p);
+
+    let p2 = Vec2D::from(p, (h, w));
+
+    let mut max_cnt = 0;
+
+    for rbit in 1..=(2_i32.pow(h as u32) - 1) {
+        let mut map = HashMap::new();
+
+        for c in 0..w {
+            let mut val = None;
+            for r in 0..h {
+                if rbit & (1 << r) != 0 {
+                    let v = p2[(r, c)];
+                    if let Some(tval) = val {
+                        if tval != v {
+                            val = None;
+                            break;
+                        }
+                    }
+                    else {
+                        val = Some(v);
+                    }
+                }
+            }
+
+            if let Some(tval) = val {
+                map.entry(tval).and_modify(|e| *e += 1).or_insert(1);
+
+                let mut rn = 0;
+                for r in 0..h {
+                    if rbit & (1 << r) != 0 {
+                        rn += 1;
+                    }
+                }
+    
+                max_cnt = max_cnt.max(map[&tval] * rn);
+            }
+        }
+
+        debug!(rbit, map);
+    }
+    
+    println!("{}", max_cnt);
+}
