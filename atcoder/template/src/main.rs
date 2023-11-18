70e49d80c7bd3315e06a8d8017e39a2ed384551e
convexbrain <convexbrain@gmail.com>
2023-11-18 20:30:18 +0900

    updated: template

--- atcoder/template/src/main.rs
+++ atcoder/template/src/main.rs
@@ -38,19 +38,22 @@ fn main() {
     std::io::stdin().read_to_string(&mut buf).unwrap();
     let mut token = buf.split_whitespace();
 
-    let a = token.next().unwrap(); // &str
-    let b = token.next().unwrap().to_string(); // String
-    let c = token.next().unwrap().as_bytes(); // &[u8]
-    let d = token.next().unwrap().as_bytes().to_vec(); // Vec<u8>
-    let n: usize = token.next().unwrap().parse().unwrap();
-
-    debug!(a, b, c, d, n);
-
-    let c0 = char::from_u32(c[0] as u32).unwrap(); // u8 -> char
-    let c = str::from_utf8(c).unwrap(); // &[u8] -> &str
-    let d = String::from_utf8(d).unwrap(); // Vec<u8> -> String
-
-    debug!(c0, c, d);
-
+    #[cfg(feature = "template")]
+    {
+        let a = token.next().unwrap(); // &str
+        let b = token.next().unwrap().to_string(); // String
+        let c = token.next().unwrap().as_bytes(); // &[u8]
+        let d = token.next().unwrap().as_bytes().to_vec(); // Vec<u8>
+        let n: usize = token.next().unwrap().parse().unwrap();
+    
+        debug!(a, b, c, d, n);
+    
+        let c0 = char::from_u32(c[0] as u32).unwrap(); // u8 -> char
+        let c = str::from_utf8(c).unwrap(); // &[u8] -> &str
+        let d = String::from_utf8(d).unwrap(); // Vec<u8> -> String
+    
+        debug!(c0, c, d);
+    }
+    
     println!("{buf}");
 }
