eb242f97cb6d014153e9dff92ae819c0bbf23758
convexbrain <convexbrain@gmail.com>
2024-01-04 15:28:41 +0900

    created: 066bn

---
+++ atcoder/typical90/typical90_066bn/src/main.rs
@@ -0,0 +1,96 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct Tokens<'a>(std::str::SplitWhitespace<'a>);
+
+#[allow(dead_code)]
+impl<'a> Tokens<'a> {
+    fn new(buf: &'a mut String) -> Self {
+        std::io::stdin().read_to_string(buf).unwrap();
+        Tokens(buf.split_whitespace())
+    }
+    fn release(self) -> std::str::SplitWhitespace<'a> {
+        self.0
+    }
+    fn next_string(&mut self) -> String {
+        self.0.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.0.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.0.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+    fn collect_index<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<(usize, T)> {
+        (0..n).map(|i| (i, self.next())).collect()
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut tokens_buf = String::new();
+    let mut tokens = Tokens::new(&mut tokens_buf);
+
+    let n: usize = tokens.next();
+    let lr: Vec<(u8, u8)> = (0..n).map(|_| (tokens.next(), tokens.next())).collect();
+    debug!(n, lr);
+
+    let mut exp = 0_f64;
+
+    for i in 0..n {
+        for j in (i + 1)..n {
+            let mut total = 0;
+            let mut cnt = 0;
+
+            for ia in lr[i].0 ..= lr[i].1 {
+                for ja in lr[j].0 ..= lr[j].1 {
+                    total += 1;
+                    if ia > ja {
+                        cnt += 1;
+                    }
+                }
+            }
+
+            exp += cnt as f64 / total as f64;
+        }
+    }
+
+    println!("{exp}");
+}
