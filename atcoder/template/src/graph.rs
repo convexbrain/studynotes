8e63654f868748eb1b879ffccc90adf5a1e3cc72
convexbrain <convexbrain@gmail.com>
2024-02-11 22:00:05 +0900

    updated: graph

--- atcoder/template/src/graph.rs
+++ atcoder/template/src/graph.rs
@@ -76,7 +76,7 @@ impl<V, W: Copy> Graph<V, W> {
         &self.node_values
     }
 
-    fn traverse<T, F>(&mut self,
+    fn _traverse<T, F>(&mut self,
         first_node: Option<usize>, first_weight: W, first_travel: T,
         unvisited: Option<BTreeSet<usize>>,
         bfs: bool,
@@ -112,6 +112,24 @@ impl<V, W: Copy> Graph<V, W> {
 
         unvis
     }
+
+    fn dfs<T, F>(&mut self,
+        first_node: Option<usize>, first_weight: W, first_travel: T,
+        unvisited: Option<BTreeSet<usize>>,
+        func: F) -> BTreeSet<usize>
+    where F: FnMut(usize, &mut V, W, T) -> T, T: Copy {
+
+        self._traverse(first_node, first_weight, first_travel, unvisited, false, func)
+    }
+    
+    fn bfs<T, F>(&mut self,
+        first_node: Option<usize>, first_weight: W, first_travel: T,
+        unvisited: Option<BTreeSet<usize>>,
+        func: F) -> BTreeSet<usize>
+    where F: FnMut(usize, &mut V, W, T) -> T, T: Copy {
+
+        self._traverse(first_node, first_weight, first_travel, unvisited, true, func)
+    }
     
     fn dijkstra<U>(&mut self,
         first_node: Option<usize>,
@@ -144,47 +162,117 @@ impl<V, W: Copy> Graph<V, W> {
         unvis
     }
     
-    fn dfs_rec<T, F1, F2>(
+    fn _dfs_rec<T, F>(
         node_values: &mut[V], node_edges: &[BTreeSet<usize>], edges: &[Edge<W>],
         u: usize, w: W, t: T,
         unvis: &mut BTreeSet<usize>,
-        func_pre: &mut F1, func_post: &mut F2)
-    where F1: FnMut(usize, &mut V, W, T) -> T, F2: FnMut(usize, &mut V), T: Copy {
+        func_pre_post: &mut F)
+    where F: FnMut(usize, &mut V, Option<(W, T)>) -> T, T: Copy {
 
         if unvis.contains(&u) {
             unvis.remove(&u);
 
-            let nt = func_pre(u, &mut node_values[u], w, t);
+            let nt = func_pre_post(u, &mut node_values[u], Some((w, t)));
 
             for &e in node_edges[u].iter() {
                 let (nu, nw) = edges[e].node_from(u);
 
-                Self::dfs_rec(node_values, node_edges, edges, nu, nw, nt, unvis, func_pre, func_post);
+                Self::_dfs_rec(node_values, node_edges, edges, nu, nw, nt, unvis, func_pre_post);
             }
 
-            func_post(u, &mut node_values[u]);
+            func_pre_post(u, &mut node_values[u], None);
         }
     }
 
-    fn dfs<T, F1, F2>(&mut self,
+    fn dfs_rec<T, F>(&mut self,
         first_node: Option<usize>, first_weight: W, first_travel: T,
         unvisited: Option<BTreeSet<usize>>,
-        func_pre: &mut F1, func_post: &mut F2) -> BTreeSet<usize>
-    where F1: FnMut(usize, &mut V, W, T) -> T, F2: FnMut(usize, &mut V), T: Copy {
+        mut func_pre_post: F) -> BTreeSet<usize>
+    where F: FnMut(usize, &mut V, Option<(W, T)>) -> T, T: Copy {
 
         let n = self.node_values.len();
         let mut unvis = unvisited.unwrap_or((0..n).collect());
 
         let first_node = first_node.unwrap_or(*unvis.first().unwrap());
 
-        Self::dfs_rec(
+        Self::_dfs_rec(
             &mut self.node_values, &self.node_edges, &self.edges,
             first_node, first_weight, first_travel,
             &mut unvis,
-            func_pre, func_post);
+            &mut func_pre_post);
 
         unvis
     }
 }
 
 //#############################################################################
+
+#[test]
+fn test_graph_dfs_bfs() {
+    let mut g = Graph::new_nodes(7, 0, true);
+    g.add_edge(0, 1, ());
+    g.add_edge(0, 2, ());
+    g.add_edge(1, 3, ());
+    g.add_edge(1, 4, ());
+    g.add_edge(2, 5, ());
+    g.add_edge(2, 6, ());
+
+    let mut cnt = 0;
+    g.dfs(None, (), (), None,
+        |_u, v, _w, _t| {
+            *v = cnt;
+            cnt += 1;
+        });
+    
+    assert_eq!(g.node_values(), [0, 4, 1, 6, 5, 3, 2]);
+
+    let mut cnt = 0;
+    g.bfs(None, (), (), None,
+        |_u, v, _w, _t| {
+            *v = cnt;
+            cnt += 1;
+        });
+    
+    assert_eq!(g.node_values(), [0, 1, 2, 3, 4, 5, 6]);
+
+    let mut cnt = 0;
+    g.dfs_rec(None, (), (), None,
+        |_u, v, wt| {
+            if let Some((_w, _t)) = wt {
+            }
+            else {
+                *v = cnt;
+                cnt += 1;
+            }
+            ()
+        },
+    );
+    
+    assert_eq!(g.node_values(), [6, 2, 5, 0, 1, 3, 4]);
+}
+
+#[test]
+fn test_graph_dfs_dijkstra() {
+    let mut g = Graph::new_nodes(6, u16::MAX, true);
+    g.add_edge(0, 1, 1);
+    g.add_edge(0, 2, 2);
+    g.add_edge(0, 3, 3);
+    g.add_edge(0, 4, 4);
+    g.add_edge(1, 5, 40);
+    g.add_edge(2, 5, 30);
+    g.add_edge(3, 5, 20);
+    g.add_edge(4, 5, 10);
+
+    g.dijkstra(Some(0), None,
+        |_u, _p, v, w| {
+            if *v > w {
+                *v = w;
+                true
+            }
+            else {
+                false
+            }
+        });
+    
+    assert_eq!(g.node_values(), [0, 1, 2, 3, 4, 14]);
+}
