17ad9be159bfca71154e503c42a6e73aa09d6f36
convexbrain <convexbrain@gmail.com>
2024-02-24 22:54:38 +0900

    created: abc342

---
+++ atcoder/abc/abc342/abc342_c/src/main.rs
@@ -0,0 +1,96 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct Tokens<'a>(std::str::SplitWhitespace<'a>);
+
+#[allow(dead_code)]
+impl<'a> Tokens<'a> {
+    fn new(buf: &'a mut String) -> Self {
+        std::io::stdin().read_to_string(buf).unwrap();
+        Tokens(buf.split_whitespace())
+    }
+    fn release(self) -> std::str::SplitWhitespace<'a> {
+        self.0
+    }
+    fn next_string(&mut self) -> String {
+        self.0.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.0.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.0.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+    fn collect_index<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<(usize, T)> {
+        (0..n).map(|i| (i, self.next())).collect()
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut tokens_buf = String::new();
+    let mut tokens = Tokens::new(&mut tokens_buf);
+
+    let n: usize = tokens.next();
+    let s = tokens.next_bytes(); // Vec<u8>
+    let q: usize = tokens.next();
+
+    let mut a: Vec<u8> = (0..26).collect();
+
+    for _ in 0..q {
+        let c = tokens.next_bytes(); // Vec<u8>
+        let d = tokens.next_bytes(); // Vec<u8>
+        let c = c[0] - b'a';
+        let d = d[0] - b'a';
+
+        for aa in a.iter_mut() {
+            if *aa == c {
+                *aa = d;
+            }
+        }
+    }
+
+    for c in s.iter() {
+        let cc = a[(*c - b'a') as usize] + b'a';
+        let cc = char::from_u32(cc as u32).unwrap(); // u8 -> char
+        print!("{cc}");
+    }
+    println!();
+}
---
+++ atcoder/abc/abc342/abc342_d/src/main.rs
@@ -0,0 +1,111 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct Tokens<'a>(std::str::SplitWhitespace<'a>);
+
+#[allow(dead_code)]
+impl<'a> Tokens<'a> {
+    fn new(buf: &'a mut String) -> Self {
+        std::io::stdin().read_to_string(buf).unwrap();
+        Tokens(buf.split_whitespace())
+    }
+    fn release(self) -> std::str::SplitWhitespace<'a> {
+        self.0
+    }
+    fn next_string(&mut self) -> String {
+        self.0.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.0.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.0.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+    fn collect_index<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<(usize, T)> {
+        (0..n).map(|i| (i, self.next())).collect()
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut tokens_buf = String::new();
+    let mut tokens = Tokens::new(&mut tokens_buf);
+
+    let n: usize = tokens.next();
+    let a: Vec<u64> = tokens.collect(n);
+
+    let mut b = vec![0; 200001];
+    for &val in a.iter() {
+        b[val as usize] += 1;
+    }
+    //debug!(b);
+
+    let mut s = Vec::new();
+    for i in 0..=200000 {
+        let ii = i * i;
+        s.push(ii);
+    }
+    //debug!(s);
+
+    let mut cnt = 0;
+    for &val in a.iter() {
+        if val == 0 {
+            cnt += n - 1;
+        }
+        else {
+            for ss in s.iter() {
+                if ss % val == 0 {
+                    let p = ss / val;
+
+                    if p <= 200000 {
+                        cnt += b[p as usize];
+                        if p == val {
+                            cnt -= 1;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    let cnt = cnt / 2;
+    println!("{cnt}");
+}
