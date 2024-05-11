1ce44ab5627982d3d86e11a49dd7d3bb43c766b0
convexbrain <convexbrain@gmail.com>
2024-05-11 22:48:46 +0900

    created: abc353

---
+++ atcoder/abc/abc353/abc353_c/src/main.rs
@@ -0,0 +1,101 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct Tokens<'a>(std::str::SplitWhitespace<'a>);
+
+#[allow(dead_code)]
+impl<'a> Tokens<'a> {
+    fn new(buf: &'a mut String) -> Self {
+        std::io::stdin().read_to_string(buf).unwrap();
+        Tokens(buf.split_whitespace())
+    }
+    fn release(self) -> std::str::SplitWhitespace<'a> {
+        self.0
+    }
+    fn next_string(&mut self) -> String {
+        self.0.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.0.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.0.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+    fn collect_index<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<(usize, T)> {
+        (0..n).map(|i| (i, self.next())).collect()
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut tokens_buf = String::new();
+    let mut tokens = Tokens::new(&mut tokens_buf);
+
+    let n: usize = tokens.next();
+    let mut a: Vec<u64> = tokens.collect(n);
+
+    let sum = a.iter().sum::<u64>() * (n as u64 - 1);
+    a.sort();
+
+    let mut s =  BTreeMap::new();
+    for (idx, ai) in a.iter().enumerate() {
+        if !s.contains_key(ai) {
+            s.insert(*ai, idx);
+        }
+    }
+    //let s: BTreeMap<u64, usize> = a.iter().copied().enumerate().map(|x| (x.1, x.0)).collect();
+    debug!(s);
+
+    let mut c = 0;
+    for ai in a.iter() {
+        if let Some(r) = s.range((100000000 - *ai)..).next() {
+            c += n - r.1;
+
+            if ai * 2 >= 100000000 {
+                c -= 1;
+            }
+        }
+    }
+    debug!(sum, c);
+    c /= 2;
+
+    let ans = sum - c as u64 * 100000000;
+    println!("{ans}");
+}
