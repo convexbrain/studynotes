13dcdc907d2395725c3b3d4a6322b9b5644782eb
convexbrain <convexbrain@gmail.com>
2024-08-03 22:48:56 +0900

    created: abc365

---
+++ atcoder/abc/abc365/abc365_d/src/main.rs
@@ -0,0 +1,112 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct Tokens<'a>(std::str::SplitWhitespace<'a>);
+
+#[allow(dead_code)]
+impl<'a> Tokens<'a> {
+    fn new(buf: &'a mut String) -> Self {
+        std::io::stdin().read_to_string(buf).unwrap();
+        Tokens(buf.split_whitespace())
+    }
+    fn release(self) -> std::str::SplitWhitespace<'a> {
+        self.0
+    }
+    fn next_string(&mut self) -> String {
+        self.0.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.0.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.0.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+    fn collect_index<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<(usize, T)> {
+        (0..n).map(|i| (i, self.next())).collect()
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut tokens_buf = String::new();
+    let mut tokens = Tokens::new(&mut tokens_buf);
+
+    let n: usize = tokens.next();
+    let s = tokens.next_string(); // String
+
+    let mut al = 0;
+    let mut pc = ' ';
+    for c in s.chars() {
+        let wc = match c {
+            'R' => {'P'},
+            'P' => {'S'},
+            'S' => {'R'},
+            _ => {panic!()},
+        };
+        if pc == wc {
+            pc = c;
+        }
+        else {
+            al += 1;
+            pc = wc;
+        }
+    }
+
+    let mut ar = 0;
+    let mut pc = ' ';
+    for c in s.chars().rev() {
+        let wc = match c {
+            'R' => {'P'},
+            'P' => {'S'},
+            'S' => {'R'},
+            _ => {panic!()},
+        };
+        if pc == wc {
+            pc = c;
+        }
+        else {
+            ar += 1;
+            pc = wc;
+        }
+    }
+
+    let a = al.max(ar);
+    println!("{a}");
+}
