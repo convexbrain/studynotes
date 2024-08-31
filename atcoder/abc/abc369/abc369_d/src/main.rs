4b2c0b79cd8bfd38bc0bc17a06f5e683f4f893f3
convexbrain <convexbrain@gmail.com>
2024-08-31 23:13:26 +0900

    created: abc369

---
+++ atcoder/abc/abc369/abc369_d/src/main.rs
@@ -0,0 +1,86 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct Tokens<'a>(std::str::SplitWhitespace<'a>);
+
+#[allow(dead_code)]
+impl<'a> Tokens<'a> {
+    fn new(buf: &'a mut String) -> Self {
+        std::io::stdin().read_to_string(buf).unwrap();
+        Tokens(buf.split_whitespace())
+    }
+    fn release(self) -> std::str::SplitWhitespace<'a> {
+        self.0
+    }
+    fn next_string(&mut self) -> String {
+        self.0.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.0.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.0.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+    fn collect_index<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<(usize, T)> {
+        (0..n).map(|i| (i, self.next())).collect()
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut tokens_buf = String::new();
+    let mut tokens = Tokens::new(&mut tokens_buf);
+
+    let n: usize = tokens.next();
+    let a: Vec<u64> = tokens.collect(n);
+
+    let mut d = vec![vec![0, 0]; n];
+    d[0][0] = 0;
+    d[0][1] = a[0];
+
+    for i in 1..n {
+        d[i][0] = d[i - 1][0].max(d[i - 1][1] + a[i] * 2);
+        d[i][1] = d[i - 1][1].max(d[i - 1][0] + a[i]);
+    }
+    debug!(d);
+
+    let ans = d[n - 1][0].max(d[n - 1][1]);
+    println!("{ans}");
+}
