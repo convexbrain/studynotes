fea3dce4edcea857e40ff94a5647a2eaa866cd84
convexbrain <convexbrain@gmail.com>
2024-12-28 23:26:38 +0900

    created: abc386

---
+++ atcoder/abc/abc386/abc386_d/src/main.rs
@@ -0,0 +1,105 @@
+use std::prelude::rust_2021::*;
+use std::io::prelude::*;
+
+#[allow(unused_imports)]
+use std::{
+    collections::*, ops::{*, Bound::*}, cmp::*,
+    rc::*, cell::*,
+};
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ( $($x:tt)* ) => {};
+}
+
+#[cfg(debug_assertions)]
+macro_rules! debug {
+    () => {
+        eprintln!("[@{}]", line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            ref tmp => {
+                eprintln!("[@{}] {} = {:?}",
+                    line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(debug!($val)),+,)
+    };
+}
+
+struct Tokens<'a>(std::str::SplitWhitespace<'a>);
+
+#[allow(dead_code)]
+impl<'a> Tokens<'a> {
+    fn new(buf: &'a mut String) -> Self {
+        std::io::stdin().read_to_string(buf).unwrap();
+        Tokens(buf.split_whitespace())
+    }
+    fn release(self) -> std::str::SplitWhitespace<'a> {
+        self.0
+    }
+    fn next_string(&mut self) -> String {
+        self.0.next().unwrap().to_string()
+    }
+    fn next_bytes(&mut self) -> Vec<u8> {
+        self.0.next().unwrap().as_bytes().to_vec()
+    }
+    fn next<T>(&mut self) -> T
+    where T: std::str::FromStr, T::Err: std::fmt::Debug {
+        self.0.next().unwrap().parse().unwrap()
+    }
+    fn collect<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+    fn collect_index<T, C>(&mut self, n: usize) -> C
+    where T: std::str::FromStr, T::Err: std::fmt::Debug, C: FromIterator<(usize, T)> {
+        (0..n).map(|i| (i, self.next())).collect()
+    }
+}
+
+//#############################################################################
+
+fn main() {
+    let mut tokens_buf = String::new();
+    let mut tokens = Tokens::new(&mut tokens_buf);
+
+    let n: usize = tokens.next();
+    let m: usize = tokens.next();
+
+    let mut xyc = Vec::new();
+
+    for _ in 0..m {
+        let x: usize = tokens.next();
+        let y: usize = tokens.next();
+        let c: char = tokens.next();
+        let x = x - 1;
+        let y = y - 1;
+
+        xyc.push((x, y, c));
+    }
+    xyc.sort();
+    debug!(xyc);
+
+    let mut w_min_y = usize::MAX;
+    for (x, y, c) in xyc.iter() {
+        match c {
+            'W' => {
+                w_min_y = w_min_y.min(*y);
+            },
+            'B' => {
+                if w_min_y <= *y {
+                    println!("No");
+                    return;
+                }
+
+            },
+            _ => {panic!()}
+        }
+    }
+    println!("Yes");
+}
