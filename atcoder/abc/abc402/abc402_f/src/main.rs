7e3b073fc0de77a52f0d1cf6634cb4c03cf7faa7
convexbrain <convexbrain@gmail.com>
2025-04-19 23:06:51 +0900

    updated

--- atcoder/abc/abc402/abc402_f/src/main.rs
+++ atcoder/abc/abc402/abc402_f/src/main.rs
@@ -63,20 +63,47 @@ impl<'a> Tokens<'a> {
 
 //#############################################################################
 
-fn sub(aa: &Vec<Vec<char>>, i: usize, j: usize, mut s: String, m: u64, n: usize) -> u64 {
-    s.push(aa[i][j]);
+fn mod_pow<N>(mut x: N, mut p: N, m: N) -> N
+where N: Default + Ord + BitAnd<Output=N> + ShrAssign + Mul<Output=N> + Rem<Output=N> + SubAssign + Copy + Div<Output=N>
+{
+    let zero = N::default();
+    let one = m / m;
+
+    if p == zero {
+        return one;
+    }
+
+    let mut k = one;
+
+    while p > one {
+        if p & one == zero {
+            x = (x * x) % m;
+            p >>= one;
+        }
+        else {
+            k = (k * x) % m;
+            p -= one;
+        }
+    }
+    (k * x) % m
+}
+
+//#############################################################################
+
+fn sub(aa: &Vec<Vec<u64>>, i: usize, j: usize, s: u64, m: u64, n: usize) -> u64 {
+    let s = (s + aa[i][j]) % m;
+
     if i == n - 1 && j == n - 1 {
-        let ss: u64 = s.parse().unwrap();
-        ss % m
+        s
     }
     else {
         let mut ss = 0;
         if i < n - 1 {
-            let s0 = sub(aa, i + 1, j, s.clone(), m, n);
+            let s0 = sub(aa, i + 1, j, s, m, n);
             ss = ss.max(s0);
         }
         if j < n - 1 {
-            let s1 = sub(aa, i, j + 1, s.clone(), m, n);
+            let s1 = sub(aa, i, j + 1, s, m, n);
             ss = ss.max(s1);
         }
         ss
@@ -90,11 +117,15 @@ fn main() {
     let n: usize = tokens.next();
     let m: u64 = tokens.next();
     let mut aa = Vec::new();
-    for _ in 0..n {
-        let a: Vec<char> = tokens.collect(n);
+    for i in 0..n {
+        let mut a: Vec<u64> = tokens.collect(n);
+        for (j, ai) in a.iter_mut().enumerate() {
+            let p = mod_pow(10, (2 * n - 2 - (i + j)) as u64, m);
+            *ai = (p * *ai) % m;
+        }
         aa.push(a);
     }
 
-    let ans = sub(&aa, 0, 0, String::new(), m, n);
+    let ans = sub(&aa, 0, 0, 0, m, n);
     println!("{ans}");
 }
